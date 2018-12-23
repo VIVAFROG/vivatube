844823bef1fa7dae18611de580c2bdb5a334fd07
VIVAFROG <vivafrog@gmail.com>
2018-12-23 23:26:17 +0900

    2.5

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require("express");
+import express from "express";
+
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-}
+
+const handleHome = (req, res) => res.send("Hello from my server");
+
+const handleProfile = (req, res) => res.send("You are ony my profile");
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
