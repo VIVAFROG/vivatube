ecb6009121b16437c8486dce213e3d5ad59efa1a
VIVAFROG <vivafrog@gmail.com>
2018-12-29 20:25:36 +0900

    2.8

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ import helmet from "helmet"; //For security
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 
-const app = express();
+import {userRouter} from "./router"
 
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
+const app = express(); 
 
 const handleHome = (req, res) => res.send("Hello from my server");
 
@@ -25,4 +22,6 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
