5d6ccd6da681c28f670851773ac25dd6d8330520
VIVAFROG <vivafrog@gmail.com>
2018-12-30 01:09:45 +0900

    2.19

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ Cloning Youtube with Vanilla and NodeJS
 M : Data
 V : How does the data look
 C : Function that looks for the data
+
+
+## Pages:
+
+- [x] Home
+- [ ] Join
+- [ ] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- routes.js
+++ routes.js
@@ -6,17 +6,19 @@ const LOGOUT = "/logout";
 const SEARCH = "/search";
 
 // Users
+
 const USERS = "/users";
-const USER_DETAIL = "/:id/";
+const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
 
 // Videos
+
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
-const DELETE_VIDEO = "/id/delete";
+const DELETE_VIDEO = "/:id/delete";
 
 const routes = {
   home: HOME,
