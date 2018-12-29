c0025fe9c83b630e9cbc067679df8e7dfc925b60
VIVAFROG <vivafrog@gmail.com>
2018-12-29 15:06:28 +0900

    2.7 middleware#2

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan"; //for logging
+import helmet from "helmet"; //For security
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -11,12 +15,11 @@ const handleHome = (req, res) => res.send("Hello from my server");
 
 const handleProfile = (req, res) => res.send("You are ony my profile");
 
-const betweenHome = (req, res, next) => {
-    console.log('Between');
-    next();
-};
-
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
