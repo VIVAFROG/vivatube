5cb0c4b00bb7b47e5168384d0fb84d636b193feb
VIVAFROG <vivafrog@gmail.com>
2018-12-29 21:27:27 +0900

    2.12 recap

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ import videoRouter from "./routers/videoRouter";
 
 const app = express();
 
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
-app.use(morgan("dev"));
+app.use(cookieParser()); // cookie 전달 받아서 사용할 수 있도록 만들어 주는 middleware
+app.use(bodyParser.json()); // 사용자가 웹사이트로 전달하는 정보들을 검사하는 middleware (from 에 담아서 업로드))
+app.use(bodyParser.urlencoded({ extended: true })); //
+app.use(helmet()); // for security
+app.use(morgan("dev")); // application 에서 발생 하는 모든 일 logging
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
